03a2f6ef24569fda41baee4f38fb7dc1ed51a371
khaled1gamal <kgamal105@gmail.com>
2025-01-25 09:56:13 +0300

    chack comments

--- index.js
+++ index.js
@@ -1,88 +1,3 @@
-
-// import "dotenv/config";
-// import fetch from "node-fetch";
-// import { Facebook } from "facebook-sdk";
-
-// const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-// const PAGE_ID = process.env.PAGE_ID;
-
-// const graph = new Facebook(ACCESS_TOKEN);
-
-// async function getNewComments() {
-//   try {
-//     //console.log(`Fetching posts for page: ${PAGE_ID}`);
-//     const posts = await fetch(
-//       `https://graph.facebook.com/v21.0/515812101950051/posts?access_token=${ACCESS_TOKEN}`
-//     ).then((res) => res.json());
-
-//     //console.log("posts data", posts.data);
-
-//     if (!posts || !posts.data) {
-//       console.error("No posts returned. Check your PAGE_ID or ACCESS_TOKEN.");
-//       return [];
-//     }
-
-//     //console.log(`Fetched posts:`, posts);
-
-//     let allComments = [];
-//     for (const post of posts.data) {
-//       //console.log(`Fetching comments for post: ${post.id}`);
-//       const comments = await fetch(
-//         `https://graph.facebook.com/v21.0/${post.id}/comments?access_token=${ACCESS_TOKEN}`
-//       ).then((res) => res.json());
-//       //console.log("comments", comments);
-//       if (comments && comments.data) {
-//         allComments = allComments.concat(comments.data);
-//       } else {
-//         console.log(`No comments found for post: ${post.id}`);
-//       }
-//     }
-//     return allComments;
-//   } catch (error) {
-//     console.error("Error retrieving comments:", error.message, error.response);
-//     return [];
-//   }
-// }
-
-// async function analyzeAndReply(comments) {
-//   for (const comment of comments) {
-//     const commentId = comment.id;
-//     const replyMessage = "This is a reply to a comment.";
-
-//     try {
-//       await fetch(
-//         `https://graph.facebook.com/v21.0/${commentId}/comments?message=replyMessage&access_token=${ACCESS_TOKEN}`,
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({
-//             message: replyMessage,
-//           }),
-//         }
-//       );
-//       //console.log(`Replied to comment ${commentId} with '${replyMessage}'`);
-//     } catch (error) {
-//       console.error(`Error replying to comment ${commentId}:`, error.message);
-//     }
-//   }
-// }
-
-// async function main() {
-//   const comments = await getNewComments();
-
-//   if (comments && comments.length > 0) {
-//     await analyzeAndReply(comments);
-//   } else {
-//     console.log("No new comments found.");
-//   }
-// }
-
-// setInterval(() => {
-//   main();
-// }, 1000);
-
 import "dotenv/config";
 import express from "express";
 import fetch from "node-fetch";
@@ -120,11 +35,21 @@ app.post("/webhooks", async (req, res) => {
     for (const entry of body.entry) {
       const changes = entry.changes || [];
       for (const change of changes) {
-        if (change.field === "feed" && change.value && change.value.item === "comment") {
+        if (
+          change.field === "feed" &&
+          change.value &&
+          change.value.item === "comment"
+        ) {
           const commentId = change.value.comment_id;
           const commentMessage = change.value.message || "";
 
           console.log(`New comment received: ${commentMessage}`);
+
+          // التحقق من أن التعليق ليس من الصفحة نفسها
+          if (commenterId === process.env.PAGE_ID) {
+            console.log(`Skipping comment ${commentId} from the page itself.`);
+            continue;
+          }
 
           // الرد على التعليق
           const replyMessage = "شكراً لتفاعلك معنا!";
@@ -139,9 +64,14 @@ app.post("/webhooks", async (req, res) => {
                 access_token: ACCESS_TOKEN,
               }),
             });
-            console.log(`Replied to comment ${commentId} with '${replyMessage}'`);
+            console.log(
+              `Replied to comment ${commentId} with '${replyMessage}'`
+            );
           } catch (error) {
-            console.error(`Error replying to comment ${commentId}:`, error.message);
+            console.error(
+              `Error replying to comment ${commentId}:`,
+              error.message
+            );
           }
         }
       }
